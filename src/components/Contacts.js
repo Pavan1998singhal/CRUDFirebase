3f88f2592091cdb91acb34eef7546a5dcbb88806
Pavan Singhal <Pavan.Singhal@smartcontrols.in>
2020-05-30 16:16:27 +0530

    first commit

---
+++ src/components/Contacts.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ContactForm from "./ContactForm";
+import firebaseDb from '../firebase';   
+
+const Contacts = () => {
+
+    const addOrEdit = obj => {
+
+    }
+
+    return ( 
+        <>   
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4 text-center">Contact Register</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-5">
+                    <ContactForm addOrEdit={addOrEdit}/>
+                </div>
+                <div className="col-row-7">
+                    <div>list of contacts</div>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default Contacts;
+
+
+//////////////////////
+/// <> is an React.Fragment
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDEBMOTDoFaIrycaDig95_tqKciLPyJOP4",
+    authDomain: "react-crud-ac5ce.firebaseapp.com",
+    databaseURL: "https://react-crud-ac5ce.firebaseio.com",
+    projectId: "react-crud-ac5ce",
+    storageBucket: "react-crud-ac5ce.appspot.com",
+    messagingSenderId: "373902570975",
+    appId: "1:373902570975:web:e68775f1d0135f1294406a"
+  };
+ 
+  // Initialize Firebase
+ var fireDb =  firebase.initializeApp(firebaseConfig);
+
+ export default fireDb.database().ref();
